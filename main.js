7f98bf75c31721b54eada8c1da872553f28ca283
soumyasrishti01 <78192188+soumyasrishti01@users.noreply.github.com>
2021-09-02 18:11:40 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+noseX = 0;
+noseY = 0;
+difference = 0;
+leftWristX = 0;
+rightWristY = 0;
+
 function setup(){
     video = createCapture(VIDEO);
     video.size(500, 380);
@@ -15,8 +21,19 @@ function modelLoaded(){
 function gotPoses(results){
     if(results.length > 0){
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX + "noseY = " + noseY);
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+        console.log("leftWristX =" + leftWristX + "rightWristX =" + rightWristX + "difference =" + difference);
     }
 }
 function draw(){
     background('#d8bdf0');
+    text("Srishti", noseX, noseY);
+    textSize(difference);
+    fill("white");
+    stroke("white");
 }
